721a5d8c4074ac6c26c93eba6c9a3d867ec02259
DharuNamikaze <dharunamikaze@gmail.com>
2025-01-23 11:30:33 +0530

    displayed wpm,mistakes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 'use client'
 import "./globals.css";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import content from "@components/content";
 import React from 'react'
 import './reset.css'
@@ -8,7 +8,7 @@ import Context from "@components/Context";
 
 export default function Page() {
   const maxTime = 60; // max time can be changed
-  const [currcontent, setCurrContent] = useState(content[ranIndex]);
+  const [currcontent, setCurrContent] = useState('');
   const [word, setWord] = useState('');
   const [charIndex, setCharIndex] = useState(0);
   const [time, setTime] = useState(maxTime);
@@ -16,65 +16,88 @@ export default function Page() {
   const [wpm, setWpm] = useState(0);
   const [cpm, setCpm] = useState(0);
   const [acc, setAcc] = useState(0); // accuracy
-  
-  useEffect(()=>{
+
+  const totalChar = useRef(0)
+  useEffect(() => {
     const ranIndex = Math.floor(Math.random() * content.length);
     setCurrContent(content[ranIndex]);
-  },[]);
-  
+  }, []);
+
   const callbackref = useCallback(InputEvent => {
     if (InputEvent) {
       document.addEventListener("keydown", () => InputEvent.focus())
     }
-  })
-  
+  }, []);
+
   const handleInput = (e) => {
     const { value } = e.target;
     setWord(value);
     setCharIndex(value.length);
-    const result = testCalculator(currcontent, value);
+
+    const { mistakes, wpm } = testCalculator(currcontent, value);
+
+    setMistakes(mistakes)
+    setWpm(wpm)
+    testAccuracy(value)
   }
 
-  const testCalculator = (originalValue, inputValue) => {
-    // Your logic to calculate test results
+  const testCalculator = (originalValue, typedvalue) => {
+
+    const mistakes = typedvalue.split('').reduce((acc, typedChar, index) => {
+      return typedChar !== originalValue[index] ? acc + 1 : acc
+    }, 0)
+
+    const cpm = typedvalue.length - mistakes
+
+    // im dont display cpm cus nobody cares
+
+    const wpm = Math.floor(cpm / 5); //1wpm = 5cpm
+
+    return { mistakes, cpm, wpm }
   }
 
-  return (
-    <div className="tab">
-      <div className="timer">
-        {time > 0 ? (
-          <>
-            <p>{time}</p>
-            <small>Seconds</small>
-          </>
-        ) : (
-          <small className="">Time's up!</small>
-        )}
-      </div>
+  function testAccuracy(value) {
+    if (value.length > charIndex) {
+      totalChar.current+=1
+      console.log(totalChar)
+    }
+  }
+    return (
+      <div className="tab">
+        <div className="timer">
+          {time > 0 ? (
+            <>
+              <p>{time}</p>
+              <small>Seconds</small>
+            </>
+          ) : (
+            <small className="">Time's up!</small>
+          )}
+        </div>
 
-      <div className="square">
-        <p>{wpm}</p>
-        <small>Word/Min</small>
-      </div>
-
+        <div className="square">
+          <p>{wpm}</p>
+          <small>Word/Min</small>
+        </div>
+        {/* 
       <div className="square">
         <p>{cpm}</p>
         <small>Char/Min</small>
-      </div>
+      </div> */}
 
-      <div className="square">
-        <p>{mistakes}</p>
-        <small>Mistakes</small>
-      </div>
+        <div className="square">
+          <p>{mistakes}</p>
+          <small>Mistakes</small>
+        </div>
 
-      <div className="square">
-        <p>{acc}</p>
-        <small>% Accuracy</small>
-      </div>
+        <div className="square">
+          <p>{acc}</p>
+          <small>% Accuracy</small>
+        </div>
 
-      <input type="text" value={word} onChange={handleInput} ref={callbackref} style={{ opacity: 0 }} />
-      <Context currcontent={currcontent} word={word} charIndex={charIndex} />
-      <span className="restart">&#x27F3;</span>
-    </div>
-  );
-}
+        <input type="text" value={word} onChange={handleInput} autoFocus ref={callbackref} style={{ opacity: 0 }} />
+        <Context currcontent={currcontent} word={word} charIndex={charIndex} />
+        <span className="restart">&#x27F3;</span>
+      </div>
+    );
+  }
